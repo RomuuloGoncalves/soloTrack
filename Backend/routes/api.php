cec2cea59710347c038ac94cb502810127baddca
RomuuloGoncalves <romulogoncalves13@gmail.com>
2026-03-25 12:19:51 -0300

    deploy

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -6,3 +6,8 @@ use App\Http\Controllers\UsuarioController;
 Route::apiResource('usuarios', UsuarioController::class);
 Route::apiResource('culturas', UsuarioController::class);
 Route::apiResource('propriedades', UsuarioController::class);
+
+
+Route::get('/hello', function () {
+    return response()->json(['message' => 'Hello World! A API esta no ar.'], 200);
+});
