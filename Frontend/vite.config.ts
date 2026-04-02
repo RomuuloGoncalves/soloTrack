40418dce62079bf7a3c147b982c05ebc417f0778
RomuuloGoncalves <romulogoncalves13@gmail.com>
2026-04-02 11:31:47 -0300

    deploy vercel

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/soloTrack/',
+  // base: '/soloTrack/',
+  base: '',
   plugins: [react()],
   server: {
     proxy: {
