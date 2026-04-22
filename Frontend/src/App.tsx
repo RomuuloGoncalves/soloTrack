2fb6ea26950c19a4d29369ac7f85ce2d15aea593
RomuuloGoncalves <romulogoncalves13@gmail.com>
2026-04-21 21:07:03 -0300

    retorno da home

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-// import Home from './Pages/public/Home/Home'
+import Home from './Pages/public/Home/Home'
 import Login from './Pages/auth/Login/Login'
 import SignUp from "./Pages/auth/SignIn/SignUp";
 import NotFoundPage from "./Pages/public/NotFoundPage/NotFoundPage";
@@ -24,7 +24,7 @@ function App() {
       <Router>
         <ProvedorAuth>
           <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/" element={<Home />} />
 
             <Route path="/login" element={
               <RotaPublica><Login /></RotaPublica>
